791f8806fd5f9b253d4986eb66e08c25ce1f31cb
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-03-09 19:03:37 +0100

    Switches from config.js to process.env (patch)

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
+const path = require('path')
 const logger = require('./lib/logger')
 const env = process.argv[2]
 
 if (env) {
+  const envFilePath = path.resolve(process.cwd(), env)
   logger('info', ['index', 'loading environment', env])
-  require('dotenv').config(env)
+  require('dotenv').config({path: envFilePath})
 } else {
   logger('warn', ['index', 'no environment loded'])
 }
 
-const config = require('./config')
 const getPosts = require('./lib/get-posts')
 const repackPosts = require('./lib/repack-post')
 const prepareForIndex = require('./lib/prepare-for-index')
 const addDocument = require('./lib/add-document')
 
 async function doIndexing () {
-  const data = await getPosts(config.API_URL)
+  const data = await getPosts(process.env.API_URL)
   const documents = data.map(repackPosts).map(prepareForIndex).map(addDocument)
   await Promise.all(documents).then(() => {
     console.log('Finished')
